e342f20301d7b1b12e4fd760cc1b2b9e973d0fb6
IureLima <iure.lima@jala.university>
2025-01-19 00:25:12 -0300

    feat: add the files for moke datas

---
+++ src/data/bookings.js
@@ -0,0 +1,31 @@
+const bookings = [
+  {
+    id: "1",
+    user_id: "201",
+    room_id: "1",
+    check_in_date: "2025-01-20",
+    check_out_date: "2025-01-25",
+    total_price: 500,
+    status: "confirmed",
+  },
+  {
+    id: "2",
+    user_id: "202",
+    room_id: "2",
+    check_in_date: "2025-02-01",
+    check_out_date: "2025-02-05",
+    total_price: 600,
+    status: "pending",
+  },
+  {
+    id: "3",
+    user_id: "203",
+    room_id: "3",
+    check_in_date: "2025-03-10",
+    check_out_date: "2025-03-15",
+    total_price: 1500,
+    status: "cancelled",
+  },
+];
+
+export default bookings;
---
+++ src/data/rooms.js
@@ -0,0 +1,28 @@
+const rooms = [
+  {
+    id: "1",
+    hotel_id: "101",
+    room_number: "101",
+    room_type: "Single",
+    price_per_night: 100,
+    is_available: true,
+  },
+  {
+    id: "2",
+    hotel_id: "102",
+    room_number: "202",
+    room_type: "Double",
+    price_per_night: 150,
+    is_available: false,
+  },
+  {
+    id: "3",
+    hotel_id: "103",
+    room_number: "303",
+    room_type: "Suite",
+    price_per_night: 300,
+    is_available: true,
+  },
+];
+
+export default rooms;
