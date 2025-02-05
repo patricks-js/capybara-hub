e975ee6b9102d9c47fe3ceb1b2f6f497fe4f9ed5
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2025-02-05 00:33:53 -0300

    chore: add todo to hotel service

--- src/modules/hotels/hotels.service.ts
+++ src/modules/hotels/hotels.service.ts
@@ -4,6 +4,14 @@ import { Model } from "mongoose";
 import { CreateHotelDTO } from "./dto/create-hotel.dto";
 import { Hotel } from "./entity/hotel.entity";
 
+/**
+ * TODO: Implement search all hotels with pagination
+ * TODO: Implement hotel by address
+ * TODO: Implement sorting
+ * TODO: Implement search by name
+ * TODO: Implement update hotel
+ */
+
 @Injectable()
 export class HotelsService {
   constructor(
