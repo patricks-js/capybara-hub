d98afe6087b2d894e185ceae3a7bf67d9a34b2ff
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2025-02-20 14:15:00 -0300

    feat: check-in date must be before check-out date and after today

--- src/modules/bookings/bookings.service.ts
+++ src/modules/bookings/bookings.service.ts
@@ -1,9 +1,13 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import dayjs from "dayjs";
 import { Model } from "mongoose";
 import { CreateBookingDto } from "./dto/create-booking.dto";
-import { Booking } from "./entities/booking.entity";
+import { Booking, BookingStatus } from "./entities/booking.entity";
 
 /**
  * TODO: Implement create booking
@@ -19,7 +23,27 @@ export class BookingsService {
   ) {}
 
   async createBooking(booking: CreateBookingDto): Promise<Booking> {
-    return this.bookingModel.create(booking);
+    const checkInDate = dayjs(booking.checkInDate);
+    const checkoutDate = dayjs(booking.checkoutDate);
+
+    const isCheckInDateBeforeCheckoutDate = checkInDate.isBefore(checkoutDate);
+    const isCheckInDateAfterToday = checkInDate.isAfter(dayjs());
+    const isCheckoutDateAfterToday = checkoutDate.isAfter(dayjs());
+
+    if (
+      !isCheckInDateBeforeCheckoutDate ||
+      !isCheckInDateAfterToday ||
+      !isCheckoutDateAfterToday
+    ) {
+      throw new BadRequestException(
+        "Check-in date must be before check-out date and after today",
+      );
+    }
+
+    return this.bookingModel.create({
+      ...booking,
+      status: BookingStatus.PENDING,
+    });
   }
 
   async cancelBooking(bookingId: string) {
